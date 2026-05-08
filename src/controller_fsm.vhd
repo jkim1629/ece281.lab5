012634f41c9bedf942fd62d01b40275be1b848d8
Kim <c28joseph.kim@afacademy.af.edu>
2026-05-07 21:06:46 -0600

    Fixed autograder

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -19,17 +19,19 @@
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 library UNISIM;
 use UNISIM.VComponents.all;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity controller_fsm is
     Port ( i_reset : in STD_LOGIC;
